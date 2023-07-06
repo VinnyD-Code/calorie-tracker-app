cc44a57621c1b7c69d1c534ebb0e144b9fe75111
Vincent <vincentt.dinh03@gmail.com>
2023-07-06 08:37:59 -0500

    added progress bar

--- tracalorie_theme/js/app.js
+++ tracalorie_theme/js/app.js
@@ -8,6 +8,9 @@ class CalorieTracker {
         this._displayCaloriesLimit();
         this._displayCaloriesTotal();
         this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 
     // Public Methods/API //
@@ -51,9 +54,41 @@ class CalorieTracker {
         caloriesBurnedEl.innerHTML = burned;
     }
 
+    _displayCaloriesRemaining() {
+        const caloriesRemainingEl = document.getElementById('calories-remaining');
+        const progressEl = document.getElementById('calorie-progress');
+
+        const remaining = this._calorieLimit - this._totalCalories;
+
+        caloriesRemainingEl.innerHTML = remaining;
+
+        if (remaining <= 0) {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+        }
+        else {
+            caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-danger');
+            caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light');
+            progressEl.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+        }
+    }
+
+    _displayCaloriesProgress() {
+        const progressEl = document.getElementById('calorie-progress');
+        const percentage = (this._totalCalories / this._calorieLimit) * 100;
+        const width = Math.min(percentage, 100);
+        progressEl.style.width = `${width}%`;
+    }
+
     _render() {
         this._displayCaloriesTotal();
         this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
+        this._displayCaloriesProgress();
     }
 }
 
@@ -75,7 +110,7 @@ class Workout {
 
 const tracker = new CalorieTracker();
 
-const breakfast = new Meal('Breakfast', 400);
+const breakfast = new Meal('Breakfast', 350);
 tracker.addMeal(breakfast);
 
 const run = new Workout('Morning Run', 300);
